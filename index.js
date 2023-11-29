22be7a3f97cb332b6060b3f1e05f8a35d3204e05
caoky9x <ky.nguyen@enish.com>
2023-11-29 15:23:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+// const server = http.createServer(app);
+// const io = socketIO(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('message', (message) => {
-        console.log(`Received: ${message}`);
+// io.on('connection', (socket) => {
+//     console.log('Client connected');
+//     socket.on('message', (message) => {
+//         console.log(`Received: ${message}`);
 
-        io.emit('message',"Server trả về " + message);
-    });
+//         io.emit('message',"Server trả về " + message);
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected');
+//     });
+// });
 
 const port = process.env.PORT || 3000;
 
